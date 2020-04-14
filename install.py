bd29d0c9a969568a9c3d64449f1d8181bb03f709
asmsuechan <suenagaryoutaabc@gmail.com>
2020-04-14 21:10:58 +0900

    Add install_apt_deps()

--- install.py
+++ install.py
@@ -27,11 +27,11 @@ def print_success(msg):
     print(bcolors.OKGREEN + msg + bcolors.ENDC)
 
 def get_ros_package_path():
-    ros_package_path = os.environ['ROS_PACKAGE_PATH']
-    paths = ros_package_path.split(':')
-
-    if len(paths) < 1:
-        sys.exit('Set ROS_PACKAGE_PATH correctly')
+    try:
+        ros_package_path = os.environ['ROS_PACKAGE_PATH']
+        paths = ros_package_path.split(':')
+    except KeyError:
+        exit_error('Set ROS_PACKAGE_PATH correctly')
 
     package_paths = []
     for path in paths:
@@ -42,50 +42,63 @@ def get_ros_package_path():
 
     return package_paths
 
-def install_path():
+def workspace_path():
     path = get_ros_package_path()
-    install_path = ""
+    workspace_path = ""
 
     if len(path) == 1:
-        install_path_confirmation = input_function("Do you really install at " + bcolors.WARNING + "\"" + path[0]  + "\""+ bcolors.ENDC + " [Y/n] ") or "Y"
-        if install_path_confirmation == "Y":
-            install_path = path[0]
-        elif install_path_confirmation == "n":
-            install_path = input_function("[Input install path] ")
+        workspace_path_confirmation = input_function("Do you really install at " + bcolors.WARNING + "\"" + path[0]  + "\""+ bcolors.ENDC + " [Y/n] ") or "Y"
+        if workspace_path_confirmation == "Y":
+            workspace_path = path[0]
+        elif workspace_path_confirmation == "n":
+            workspace_path = input_function("[Input install path] ")
         else:
             exit_error("Invalid input, aborted.")
     else:
         print("Multiple package paths are specified.")
         for p in path:
             print("\"" + p + "\"")
-        install_path = input_function("[Input install path] ")
+        workspace_path = input_function("[Input install path] ")
 
-    if len(install_path) == 0:
+    if len(workspace_path) == 0:
         exit_error("You have to specify 1 install path, aborted.")
-    return install_path
+    return workspace_path
 
-def clone(install_path):
-    if os.path.exists(install_path + "/rowma_ros"):
+def clone(workspace_path):
+    rowma_ros_path = os.path.join(workspace_path, "/rowma_ros")
+    if os.path.exists(rowma_ros_path):
         exit_error("You already have rowma_ros package, aborted.")
-    clone_args = ["clone", "https://github.com/rowma/rowma_ros.git", install_path + "/rowma_ros"]
+    clone_args = ["clone", "https://github.com/rowma/rowma_ros.git", rowma_ros_path]
     return subprocess.check_call(["git"] + list(clone_args))
 
-def pip_install(install_path):
-    pip_args = ["install", "-r", install_path + "/rowma_ros/requirements.txt"]
+def pip_install(workspace_path):
+    requirements_txt_pat = os.path.join(workspace_path, "/rowma_ros/requirements.txt")
+    pip_args = ["install", "-r", requirements_txt_pat]
     return subprocess.check_call(["pip"] + list(pip_args))
 
-def catkin_make(install_path):
-    return subprocess.check_call(["catkin_make"], shell=True, cwd=install_path + "/..")
+def install_apt_deps():
+    try:
+        distro = os.environ['ROS_DISTRO']
+    except KeyError:
+        exit_error('Set ROS_DISTRO correctly.')
+
+    return subprocess.check_call(["sudo", "apt-get", "install", "ros-" + distro + "-rosbridge-server"])
+
+def catkin_make(workspace_path):
+    working_dir = os.path.join(workspace_path, "/..")
+    return subprocess.check_call(["catkin_make"], shell=True, cwd=working_dir)
 
 def install():
     try:
-        path = install_path()
+        path = workspace_path()
         clone(path)
-        print_success("rowma_ros was successfuly downloaded at " + path)
+        print_success("rowma_ros was successfully downloaded at " + path)
         pip_install(path)
-        print_success("Dependent packages are installed successfuly.")
+        print_success("Pip dependencies were installed successfully.")
+        install_apt_deps()
+        print_success("Apt packages were installed successfully.")
         catkin_make(path)
-        print_success("rowma_ros was successfuly installed!")
+        print_success("rowma_ros was successfully installed!")
     except KeyboardInterrupt:
         exit_error("\nUser interruptation occurred.")
 
