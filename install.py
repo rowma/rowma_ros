4f543d4515a81f07b1bb494552d95b93630981e0
asmsuechan <suenagaryoutaabc@gmail.com>
2020-04-16 20:22:00 +0900

    Fix clone path when user choose to override the existing package directory

--- install.py
+++ install.py
@@ -77,7 +77,7 @@ def clone(workspace_path):
         clone_confirmation = input_function("rowma_ros already exists, do you override it? [y/N] ") or "N"
         if clone_confirmation == "y":
             _clone_into("/tmp/rowma_ros")
-            subprocess.check_call(["mv", "-f", "/tmp/rowma_ros", rowma_ros_path])
+            subprocess.check_call(["mv", "-f", "/tmp/rowma_ros", workspace_path])
         else:
             print_warning("Skipped git clone rowma_ros.")
     else:
